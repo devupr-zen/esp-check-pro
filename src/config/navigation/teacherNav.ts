1d90809c187fc4e747d29ff40c837a1b0ba53aa2
devupr-zen <upraizenlabs@gmail.com>
2025-08-18 09:37:18 +0200

    Update teacherNav.ts

--- src/config/navigation/teacherNav.ts
+++ src/config/navigation/teacherNav.ts
@@ -1,7 +1,4 @@
-// src/config/navigation/teacherNav.ts
 import { teacherNav } from "../navigation"
 export default teacherNav
 export { teacherNav }
-
-// Legacy shape for older code: [{ label, icon, href }]
 export const TEACHER_NAV = teacherNav.map(({ label, icon, href }) => ({ label, icon, href }))
